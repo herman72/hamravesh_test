00e684eda0149dc6ae5e5b686881f9e73ce473d9
Mohammad Sherafati <mohammad@mohammad-2.local>
2022-12-06 23:07:15 +0330

    create model and first scrach of views

--- hamravesh_project/dockerun/models.py
+++ hamravesh_project/dockerun/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class DockerAppp(models.Model):
+    envs = 0
+    command = 0
+
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=1024)
+    envs = models.JSONField()
--- hamravesh_project/dockerun/views.py
+++ hamravesh_project/dockerun/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def create_docker(request):
 
+def create_docker(request):
+    return HttpResponse
+
+
+def get_app(request):
+    return HttpResponse
+
+
+def del_app(request):
+    return HttpResponse
+
+
+def edit_app(request):
+    return HttpResponse
