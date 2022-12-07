4fcaccb5496304856a615efe859b488c17808095
herman72 <m.sherafati7@gmail.com>
2022-12-07 17:23:20 +0330

    add multiple views

--- hamravesh_project/dockerun/urls.py
+++ hamravesh_project/dockerun/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.create_docker, name="create_dockerun"),
+    path("create/", views.create_docker, name="create_dockerun"),
+    path("list/", views.get_list_app, name="get_list_dockerun"),
+    path("get/", views.get_app, name="get_dockerun"),
+    path("del/", views.del_app, name="del_dockerun"),
+    path("edit/", views.edit_app, name="edit_dockerun"),
+    path("run_app/", views.create_docker, name="runApp_dockerun"),
 ]
--- hamravesh_project/dockerun/views.py
+++ hamravesh_project/dockerun/views.py
@@ -6,12 +6,25 @@ from models import DockerApp
 
 def create_docker(request):
     if request.method != "POST":
-        return JsonResponse("", status=400)
+        return JsonResponse({""}, status=400)
     else:
+        app_detail = DockerApp()
         name = request.Post.get("name")
+        image = request.Post.get("image")
+        envs = request.Post.get("evs")
+        command = request.Post.get("command")
 
-        DockerApp(name=name, )
+        app_detail.name = name
+        app_detail.image = image
+        app_detail.envs = envs
+        app_detail.command = command
 
+        app_detail.save()
+
+        return JsonResponse({""}, status=200)
+
+
+def get_list_app(request):
     return HttpResponse
 
 
@@ -24,4 +37,8 @@ def del_app(request):
 
 
 def edit_app(request):
+    return HttpResponse
+
+
+def run_app(request):
     return HttpResponse
